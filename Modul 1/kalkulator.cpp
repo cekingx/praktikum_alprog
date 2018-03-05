1ba2cb771d329b96a86227a6343bc4683f705269
Dirga <cekingx@github.com>
2018-03-05 11:30:43 +0800

    Kalkulator

--- Modul 1/kalkulator.cpp
+++ Modul 1/kalkulator.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World!";
+float tambah(float a, float b);
+float kurang(float a, float b);
+float kali(float a, float b);
+float bagi(float a, float b);
+int mod(float a, float b);
 
+
+int main(){
+    float x, y;
+
+    cout << "Penjumlahan" << endl;
+    cin >> x >> y;
+    cout << mod(x,y);
     return 0;
+}
+
+float tambah(float a, float b){
+    float c;
+
+    c=a+b;
+    return c;
+}
+
+float kurang(float a, float b){
+    float c;
+
+    c=a-b;
+    return c;
+}
+
+float kali(float a, float b){
+    float c;
+
+    c=a*b;
+    return c;
+}
+
+float bagi(float a, float b){
+    float c;
+
+    c=a/b;
+    return c;
+}
+
+int mod(float a, float b){
+    int c,x,y;
+    x=(int)a;
+    y=(int)b;
+
+    c=x%y;
+    return c;
 }
