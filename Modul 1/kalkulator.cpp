b3338ce5483353be902b26f0c1c9272d47f3f06a
Dirga <cekingx@github.com>
2018-03-05 12:16:46 +0800

    Kalkulator

--- Modul 1/kalkulator.cpp
+++ Modul 1/kalkulator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 float tambah(float a, float b);
@@ -6,15 +7,61 @@ float kurang(float a, float b);
 float kali(float a, float b);
 float bagi(float a, float b);
 int mod(float a, float b);
+void menu();
 
 
 int main(){
-    float x, y;
-
-    cout << "Penjumlahan" << endl;
-    cin >> x >> y;
-    cout << mod(x,y);
+    menu();
     return 0;
+}
+
+void menu(){
+    float x,y;
+    int menu;
+    char again;
+
+    do{
+        cout << "Kalkulator ne bro. Pilih menu malu : " << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Sisa hasil bagi" << endl;
+        cout << "Pilihan > "; cin >> menu;
+        cout << "Masukkan bilangan yang akan di operasikan" << endl;
+        cin >> x >> y;
+        switch(menu){
+            case 1:{
+                cout << tambah(x,y) << endl;
+                cout << "Hitung lagi [y/t] > "; cin >> again;
+                break;
+            }
+
+            case 2:{
+                cout << kurang(x,y) << endl;
+                cout << "Hitung lagi [y/t] > "; cin >> again;
+                break;
+            }
+
+            case 3:{
+                cout << kali(x,y) << endl;
+                cout << "Hitung lagi [y/t] > "; cin >> again;
+                break;
+            }
+
+            case 4:{
+                cout << bagi(x,y) << endl;
+                cout << "Hitung lagi [y/t] > "; cin >> again;
+                break;
+            }
+
+            case 5:{
+                cout << mod(x,y) << endl;
+                cout << "Hitung lagi [y/t] > "; cin >> again;
+                break;
+            }
+        }
+    }while(again == 'y' || again == 'Y');
 }
 
 float tambah(float a, float b){
